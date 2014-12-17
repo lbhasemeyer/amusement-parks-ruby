37b5fcba729fdebddc710c34e851cf2ec732fa2c
lbhasemeyer <lbhasemeyer@gmail.com>
2014-12-17 11:38:25 -0700

    finished exercise3

--- bin/exercise2.rb
+++ bin/exercise2.rb
@@ -6,8 +6,8 @@ parks = YAML.load(File.read('data/amusement_parks.yml'))
 
   result = {}
   parks.each do |park|
-    result[park[:country]] = result[park[:country]] || []
+    result["#{park[:country]}"] = result["#{park[:country]}"] || []
     # result[:1] = result[:1] || []
-    result[park[:country]] << park
+    result["#{park[:country]}"] << park
   end
   pp result
